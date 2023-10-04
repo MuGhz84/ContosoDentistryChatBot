b4908241ebcbf2cd20cbb707748aee36de0f4af6
Mohamed Ghazal <ghazal.oca@gmail.com>
2023-10-04 06:48:47 +0300

    zxx

--- bot.js
+++ bot.js
@@ -4,34 +4,37 @@
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
 //const { QnAMaker } = require('botbuilder-ai');
+const { CustomQuestionAnswering } = require('botbuilder-ai');
 const DentistScheduler = require('./dentistscheduler');
 const IntentRecognizer = require("./intentrecognizer")
 
 class DentaBot extends ActivityHandler {
-    constructor(configuration, qnaOptions) {
+    constructor(configuration, custquestOptions) {
         // call the parent constructor
         super();
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
         // create a QnAMaker connector
         //this.QnAMaker = new QnAMaker()
+        this.customqAnswering = new CustomQuestionAnswering(configuration.CustQuestLangConfiguration, custquestOptions);
        
         // create a DentistScheduler connector
+        this.dentistScheduler = new DentistScheduler(configuration.SchedulerConfiguration);
       
         // create a IntentRecognizer connector
+        this.cluintentRecognizer = new IntentRecognizer(configuration.CLUConfiguration);
 
-        this.onMessage(async (context, next) => {     
-            const replyText = `Echo: ${ context.activity.text }`;
-            console.log('user said ' + context.activity.text)
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
+
+        this.onMessage(async (context, next) => {
 
             // send user input to QnA Maker and collect the response in a variable
-            // don't forget to use the 'await' keyword
+            const custquestResults = await this.customqAnswering.getAnswers(context);
+            
           
             // send user input to IntentRecognizer and collect the response in a variable
-            // don't forget 'await'
+            const cluResult = await this.cluintentRecognizer.executeCLUQuery(context);
+            let intent = "";
+            let entities = [];
                      
             // determine which service to respond with based on the results from LUIS //
 
@@ -43,7 +46,77 @@ class DentaBot extends ActivityHandler {
             // }
             // else {...}
              
-            //await next();
+            if (cluResult["result"]["prediction"]["intents"][0]["confidenceScore"] >= this.cluintentRecognizer.config.ConfidenceScoreThreshold) {
+                intent = cluResult["result"]["prediction"]["topIntent"];
+                entities = cluResult["result"]["prediction"]["entities"];
+                console.log('intent')
+                console.log(intent)
+                console.log('confidenceScore')
+                console.log(cluResult["result"]["prediction"]["intents"][0]["confidenceScore"])
+            } else {
+                intent = "None";
+            }
+
+            switch(intent) {
+                case 'GetAvailability': {
+                    console.log("Looking for availability")
+                    if(entities.length === 0){
+                        //const invaliddatetimerangevalue = "I will try to check for availability in the time range provided. However, I can't recognize the date/time you entered... ";
+                        //console.log(invaliddatetimerangevalue)
+                        //await context.sendActivity(invaliddatetimerangevalue);
+                        const availabilities = await this.dentistScheduler.getAvailability();
+                        console.log(availabilities)
+                        await context.sendActivity(availabilities);
+                    }
+                    else{
+                        const datetime = entities[0].text;
+                        // call api with datetime entity info
+                        const getavailabilityindatetimerange = "I'll check an available date at " + datetime;
+                        console.log(getavailabilityindatetimerange)
+                        await context.sendActivity(getavailabilityindatetimerange);
+                        const availabilities = await this.dentistScheduler.getAvailability();
+                        console.log(availabilities)
+                        await context.sendActivity(availabilities);
+                    }
+                    
+                }
+                break;
+                case 'ScheduleAppointment': {
+                    console.log("Scheduling an Appointment")
+                    if(entities.length === 0){
+                        const invaliddatetimerangevalue = "I can't recognize the date/time you entered, please enter a valid date and time in format 00:00AM/PM - DD/MM/YYYY ";
+                        console.log(invaliddatetimerangevalue)
+                        await context.sendActivity(invaliddatetimerangevalue);
+                    }
+                    else{
+                        const datetime = entities[0].text;
+                        // call api with location entity info
+                        const getScheduleAppointment = "I will schedule appointment at " + datetime;
+                        console.log(getScheduleAppointment)
+                        await context.sendActivity(getScheduleAppointment);
+                        const responseText = await this.dentistScheduler.scheduleAppointment(datetime);
+                        console.log(responseText)
+                        await context.sendActivity(responseText);
+                    }
+                    
+                }
+                break;
+                case 'None': {
+                    // If an answer was received from QnA Maker, send the answer back to the user.
+                    if (custquestResults[0]) {
+                        console.log(custquestResults[0])
+                        await context.sendActivity(`${custquestResults[0].answer}`);
+                    }
+                    else {
+                        // If no answers were returned from QnA Maker, reply with help.
+                        await context.sendActivity(`I'm not sure I found an answer to your question'
+                        + 'You can ask me questions about our dental clinik services and dentistry in general like "how can I charge my car?"
+                        + \n\n OR I can schedule an appointment with one of our dentists`);
+                    }
+                }
+                break;
+            }
+            await next();
     });
 
         this.onMembersAdded(async (context, next) => {
--- intentrecognizer.js
+++ intentrecognizer.js
@@ -1,16 +1,14 @@
-const {LuisRecognizer} = require('botbuilder-ai')
+
+var request = require('request');
 
 class IntentRecognizer {
     constructor(config) {
-        const luisIsConfigured = config && config.applicationId && config.endpointKey && config.endpoint;
-        if (luisIsConfigured) {
-            // Set the recognizer options depending on which endpoint version you want to use e.g v2 or v3.
-            // More details can be found in https://docs.microsoft.com/en-gb/azure/cognitive-services/luis/luis-migration-api-v3
-            const recognizerOptions = {
-                apiVersion: 'v3'
-            };
-
-            this.recognizer = new LuisRecognizer(config, recognizerOptions);
+        const cluIsConfigured = config && config.CLUAPIKey && config.CLUProjectName && config.CLUDeploymentName && config.CLU_StringIndexType && config.CLUPredicURL;
+        if (cluIsConfigured) {
+            this.isCLUConfigured = true;
+            this.config = config;
+        } else {
+            this.isCLUConfigured = false;
         }
     }
 
@@ -22,8 +20,45 @@ class IntentRecognizer {
      * Returns an object with preformatted LUIS results for the bot's dialogs to consume.
      * @param {TurnContext} context
      */
-    async executeLuisQuery(context) {
-        return await this.recognizer.recognize(context);
+    async executeCLUQuery(context) {
+        return new Promise((resolve, reject) => {
+            var CLU_ProjectName = this.config.CLUProjectName;
+            var CLU_DeploymentName = this.config.CLUDeploymentName;
+            var CLUPredicURL = this.config.CLUPredicURL;
+            var CLU_SubscriptionKey = this.config.CLUAPIKey;
+            var CLU_StringIndexType = this.config.CLU_StringIndexType;
+            //var url = CLUPredicURL + "language/:analyze-conversations?api-version=2022–10–01-preview";
+            var options = {
+                'method': 'POST',
+                'url': CLUPredicURL,
+                'headers': {
+                    'Ocp-Apim-Subscription-Key': CLU_SubscriptionKey,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "kind": "Conversation",
+                    "analysisInput": {
+                        "conversationItem": {
+                            "id": "1",
+                            "participantId": "1",
+                            "text": context._activity.text
+                        }
+                    },
+                    "parameters": {
+                        "projectName": CLU_ProjectName,
+                        "deploymentName": CLU_DeploymentName,
+                        "stringIndexType": CLU_StringIndexType
+                    }
+                })
+            };
+            request(options, function (error, response) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(JSON.parse(response.body));
+                }
+            });
+        });
     }
 
  
